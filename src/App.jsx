85881b230ea31daa382697d6c69f195ca5bbd07f
Mahes <mahesavpm123@gmail.com>
2025-09-27 09:11:49 +0700

    update: layout, Ui color

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import CardProfile from './components/Card'
 const profilesData = [
   {
     avatar: "https://i.pravatar.cc/150?img=12",
-    name: "Al Ghaza",
+    name: "Fadhil Al Ghaza",
     bio: "Love God",
-    age: 20
+    job: "frontend"
   },
   {
     avatar: "https://i.pravatar.cc/150?img=34",
     name: "Sakura Tanaka",
     bio: "Dreaming big, living simple",
-    age: 22
+    job: "frontend"
   },
   {
     avatar: "https://i.pravatar.cc/150?img=56",
     name: "Michael Rivera",
     bio: "Coding & coffee addict",
-    age: 25
+    job: "frontend"
   }
 ];
 
@@ -26,14 +26,14 @@ const profilesData = [
 function App() {
   return (
     <>
-      <div className='border-2 border-red-500 p-4 h-screen max-w-screen flex flex-col gap-4'>
+      <div className='border-2 border-red-500 p-4 flex flex-wrap gap-4'>
         {profilesData.map((profile) => {
         return ( 
             <CardProfile 
               avatar={profile.avatar}
               name={profile.name}
               bio={profile.bio}
-              age={profile.age}
+              job={profile.job}
             />
           );
         })}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,18 @@
-// type ProfileData = {
-//  avatar: String;
-//  name: String;
-//  bio: String;
-//  age: Number;
-// }
-
-
-const CardProfile = ({ avatar, name, bio, age}) => {
+const CardProfile = ({ avatar, name, bio, job}) => {
 
     return (
-        <div className="w-52 bg-red-200 py-6 px-5 rounded-md flex flex-col justify-center items-center gap-4">
-            <img src={avatar} alt='avatar' className='w-20 h-20 bg-white rounded-full flex justify-center items-center'/>
-            <p className='text-red-500'>{name}</p>
-            <p className='text-red-500'>{bio} </p>
-            <p className='text-red-500'>{age} Tahun</p>
+        <div className="w-[250px]max-w-sm bg-white shadow-lg border-2 border-gray-300 py-6 px-5 rounded-md flex flex-col justify-center items-center">
+            <div className="avatar w-full h-full">
+                <img src={avatar} alt='avatar' className=" w-full h-full rounded-sm"/>
+            </div>
+            <div className="w-full text-start py-2">
+                <p className='text-gray-400 font-light text-xs'>{job} </p>
+                <p className='text-gray-900 text-3xl font-medium tracking-tight leading-none'>{name}</p>
+            </div>
+            <div class="w-full h-[1px] bg-gray-900 mt-4 mb-2"></div>
+            <div className="footer w-full flex justify-between text-start">
+                <p className='text-gray-400 font-light text-xs'>{bio}</p>
+            </div>
         </div>
     )
 }
