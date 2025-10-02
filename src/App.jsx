0508eac849dc561719cc89578cad24431b3678b3
Mahes <mahesavpm123@gmail.com>
2025-10-02 15:02:06 +0700

    refactor: update CardProfile component layout and styles, comment out unused profile data

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,31 @@ import './App.css'
 import CardProfile from './components/Card'
 
 const profilesData = [
-  {
-    avatar: "https://i.pravatar.cc/150?img=12",
-    name: "Fadhil Al Ghaza",
-    bio: "Love God",
-    job: "frontend"
-  },
+  // {
+  //   avatar: "https://i.pravatar.cc/150?img=12",
+  //   name: "Fadhil Al Ghaza",
+  //   bio: "LDreaming big, living simple",
+  //   job: "frontend"
+  // },
   {
     avatar: "https://i.pravatar.cc/150?img=34",
     name: "Sakura Tanaka",
     bio: "Dreaming big, living simple",
     job: "frontend"
   },
-  {
-    avatar: "https://i.pravatar.cc/150?img=56",
-    name: "Michael Rivera",
-    bio: "Coding & coffee addict",
-    job: "frontend"
-  }
+  // {
+  //   avatar: "https://i.pravatar.cc/150?img=56",
+  //   name: "Michael Rivera",
+  //   bio: "Coding & coffee addict",
+  //   job: "frontend"
+  // }
 ];
 
 
 function App() {
   return (
     <>
-      <div className='border-2 border-red-500 p-4 flex flex-wrap gap-4'>
+      <div className='w-screen h-screen flex flex-col items-center'>
         {profilesData.map((profile) => {
         return ( 
             <CardProfile 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,45 @@
 const CardProfile = ({ avatar, name, bio, job}) => {
 
     return (
-        <div className="w-[250px]max-w-sm bg-white shadow-lg border-2 border-gray-300 py-6 px-5 rounded-md flex flex-col justify-center items-center">
-            <div className="avatar w-full h-full">
-                <img src={avatar} alt='avatar' className=" w-full h-full rounded-sm"/>
-            </div>
-            <div className="w-full text-start py-2">
-                <p className='text-gray-400 font-light text-xs'>{job} </p>
-                <p className='text-gray-900 text-3xl font-medium tracking-tight leading-none'>{name}</p>
-            </div>
-            <div class="w-full h-[1px] bg-gray-900 mt-4 mb-2"></div>
-            <div className="footer w-full flex justify-between text-start">
-                <p className='text-gray-400 font-light text-xs'>{bio}</p>
+        <div className="shadow-lg border-2 border-gray-300 flex w-[400px] h-[600px] p-2 flex-col items-start gap-[10px] rounded-4xl">
+            <div className="relative flex flex-col justify-end flex-1 self-stretch ">
+                    <img 
+                    src={avatar} 
+                    alt="avatar" 
+                    className="w-full h-full rounded-4xl object-cover"
+                    />
+                <div className="absolute bottom-0 w-full bg-white/15 shadow-lg backdrop-blur-md border border-t-0 border-white/20 flex-col items-start p-4 rounded-4xl gap-1">
+                    <div className="text-start mb-4">
+                        <p className="text-primary text-2xl font-semibold uppercase py-2">{name}</p>
+                        <p className="text-primary font-light text-md">{bio}</p>
+                    </div>
+                    <div className="flex w-full justify-between items-center">
+                        <p className="text-primary font-light text-md">liked</p>
+                        <p className="text-primary font-light text-md">User</p>
+                        <button className="bg-blue-400 py-2 px-4 rounded-2xl self-stretch">Lorem</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
 }
+
+{/* <img src={avatar} alt="avatar" className="absolute inset-0 w-full h-full object-cover" />
+			<div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 to-black/0 p-3">
+				<p className="text-white font-medium text-sm">{job}</p>
+			</div> */}
+
+            // <div className="avatar w-full h-full">
+            //     <img src={avatar} alt='avatar' className=" w-full h-full rounded-sm"/>
+            // </div>
+            // <div className="w-full text-start py-2">
+            //     <p className='text-gray-400 font-light text-xs'>{job} </p>
+            //     <p className='text-gray-900 text-3xl font-medium tracking-tight leading-none'>{name}</p>
+            // </div>
+            // <div class="w-full h-[1px] bg-gray-900 mt-4 mb-2"></div>
+            // <div className="footer w-full flex justify-between text-start">
+            //     <p className='text-gray-400 font-light text-xs'>{bio}</p>
+            // </div>
 
 export default CardProfile
